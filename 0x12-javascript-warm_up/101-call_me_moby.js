d271ffd3583701bfee2dd8301cc04a8bffafdb08
kiychu1225 <akliderje72@gmail.com>
2024-03-11 11:52:20 -0700

    i did it

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+};
+#!/usr/bin/node
+
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+};
+
